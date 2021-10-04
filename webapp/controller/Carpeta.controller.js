cbb23d3b79b7a82347b1385bbf658f436aec8bbb
Adrián Schivo <adrian.schivo@softtek.com>
2021-10-04 18:51:41 +0000

    4/10/2021

--- webapp/controller/Carpeta.controller.js
+++ webapp/controller/Carpeta.controller.js
@@ -21,51 +21,93 @@ sap.ui.define([
 		 * @public
 		 */
 		onInit : function () {
-
             var sPath = "PROCEDIMIENTOS_E_INSTRUCTIVOS";
 
 			var oViewModel,			
 			oViewModel = new JSONModel({
                 rootUrl: "",
-                rootFolder: "",
+                rootFolderPath: "",
                 repositoryId: "",
                 repositoryName: sPath,
                 repositoryDescription: "",
 			});
 			this.getView().setModel(oViewModel, "viewModel");
+            
             this.createFolderModel();
             this.setRepoUrl();
+            this.addBreadcumbPath(sPath);
+           
+		},
 
+		onPress : function (oEvent) {
+            var oContext = oEvent.getSource().getBindingContext("folderModel");
+            var name = this.getCmisObjectName(oContext);
+            var objectType = this.getCmisObjectType(oContext);
+            var objectId = this.getCmisObjectId(oContext);
+            //sap.m.MessageToast.show(name + " - " + objectType + " - " + objectId);
+
+            if (objectType === "cmis:folder") {
+                this.addBreadcumbPath(name);
+                var sPath = this.getCurrentFullPath();
+                this.getFolderObjects(sPath);
+                //sap.m.MessageToast.show(sPath);
+            } else if (objectType === "cmis:document") {
+                //sap.m.MessageToast.show("Document");
+                //download document
+            } else {
+                sap.m.MessageToast.show("Object not found");
+            }
+
+		},
+        
+        onBreadcrumbPress: function (oEvent, sLinkText) {
+			var oLink = oEvent.getSource();
+			var oBreadCrumb = this.byId("breadcrumb");
+			var iIndex = oBreadCrumb.indexOfLink(oLink);
+			var aCrumb = oBreadCrumb.getLinks().slice(iIndex + 1);
+			if (aCrumb.length) {
+				aCrumb.forEach(function(oLink) {
+					oLink.destroy();
+				});
+			} 
+            var sPath = this.getCurrentFullPath();
+            this.getFolderObjects(sPath);
+            sap.m.MessageToast.show(sPath); 
+
+		},
+
+        getCurrentFullPath: function () {
+            var sFullPath = "";
+            var sBreadcumbPath = this.getBreadcumbPath();
+            var sRootPath = this.getView().getModel("viewModel").getProperty("/rootFolderPath");
+            sFullPath = sRootPath + sBreadcumbPath;
+            return sFullPath;
+
+        },
+
+        addBreadcumbPath: function (sPath) {
             var oBreadCrumb = this.byId("breadcrumb");
 			var oLink = new Link({
 				text: sPath,
 				press:[sPath, this.onBreadcrumbPress, this]
 			});
 			oBreadCrumb.addLink(oLink);
+        },
 
-		},
-
-		onPress : function (oEvent) {
-            var oContext = oEvent.getSource().getBindingContext("folderModel");
-            //var sName = this.getCmisObjectValue(oContext, "cmis:name");
-            var name = this.getCmisObjectName(oContext);
-            var objectType = this.getCmisObjectType(oContext);
-            var objectId = this.getCmisObjectId(oContext);
-            sap.m.MessageToast.show(name + " - " + objectType + " - " + objectId);
-
-            if (objectType === "cmis:folder") {
-                var sPath = this.getView().getModel("viewModel").getProperty("/currentPath") + "/" + name;
-                this.getFolderObjects(sPath);
-                //this.getFolderObjects("F");
-            } else if (objectType === "cmis:document") {
-                //sap.m.MessageToast.show("Document");
+        getBreadcumbPath: function () {
+            var aLinks = [];
+            var sPath = "";
+            aLinks = this.getBreadcumbLinks();
+            for (var i=1; i < aLinks.length; i++) {
+                sPath = sPath +"/" + aLinks[i].getProperty("text");
             }
-            
+            return sPath;
+        },
 
-			// The source is the list item that got pressed
-			//this._showObject(oEvent.getSource());
-		},
-
+        getBreadcumbLinks: function () {
+            var oBreadCrumb = this.byId("breadcrumb");
+			return oBreadCrumb.getLinks();
+        },
 
         getCmisObjectName: function (oContext) {
             return this.getCmisObjectValue(oContext, "cmis:name");
@@ -85,12 +127,10 @@ sap.ui.define([
             return oCmisObject.object.properties[sTag].value;           
         },
 
-
         createFolderModel: function () {
             var oModel = new JSONModel();
             this.getView().setModel(oModel, "folderModel");
         },
-
 
         getDMSUrl: function (sPath) {
             var sComponent = this.getOwnerComponent().getManifest()["sap.app"]["id"]
@@ -119,9 +159,9 @@ sap.ui.define([
                 } else {
                     oResponse.loadData("../localService/xfolders.json");
                 }
-                //resolve(oResponse.getJSON());
 
             });
+
 
         },
 
@@ -134,8 +174,8 @@ sap.ui.define([
                 var url = this.getDMSUrl("/SDM_API/browser/" + root);
                 //
                 that.getView().getModel("viewModel").setProperty("/rootUrl", url);
-                that.getView().getModel("viewModel").setProperty("/rootFolder", root);
-                that.getView().getModel("viewModel").setProperty("/currentPath", root);
+                that.getView().getModel("viewModel").setProperty("/rootFolderPath", root);
+                //that.getView().getModel("viewModel").setProperty("/currentPath", root);
                 that.getView().getModel("viewModel").setProperty("/repositoryId", response[repos[0]].repositoryId);
                 that.getView().getModel("viewModel").setProperty("/repositoryName", response[repos[0]].repositoryName);
                 that.getView().getModel("viewModel").setProperty("/repositoryDescription", response[repos[0]].repositoryDescription);
@@ -152,34 +192,18 @@ sap.ui.define([
         },
 
 
-
-        onBreadcrumbPress: function (oEvent, sPath) {
-			var oLink = oEvent.getSource();
-			var oBreadCrumb = this.byId("breadcrumb");
-			var iIndex = oBreadCrumb.indexOfLink(oLink);
-			var aCrumb = oBreadCrumb.getLinks().slice(iIndex + 1);
-			if (aCrumb.length) {
-				aCrumb.forEach(function(oLink) {
-					oLink.destroy();
-				});
-				//this._setAggregation(sPath);
-			}
-		},
-
-
-        getAdjuntos: function (oResponse) {
-            var aObjects = [];
-            for(var i=0; i < oResponse.objects.length; i++) {
-                var oFolderContent = {};
-                var oObjectAttributes = {};
-                oFolderContent = oResponse.objects[i].object.properties;
-                oObjectAttributes.name = oFolderContent["cmis:name"].value;
-                oObjectAttributes.type = oFolderContent["cmis:objectTypeId"].value; 
-                aObjects.push(oObjectAttributes);
-            }
-            return aObjects;
-
-        },
+        //getAdjuntos: function (oResponse) {
+        //    var aObjects = [];
+        //    for(var i=0; i < oResponse.objects.length; i++) {
+        //        var oFolderContent = {};
+        //        var oObjectAttributes = {};
+        //        oFolderContent = oResponse.objects[i].object.properties;
+        //        oObjectAttributes.name = oFolderContent["cmis:name"].value;
+        //        oObjectAttributes.type = oFolderContent["cmis:objectTypeId"].value; 
+        //        aObjects.push(oObjectAttributes);
+        //    }
+        //    return aObjects;
+        //},
 
 
 		
