3f66d813053298ccc30136400c89132142d6b9cc
Adrian S <adrian.schivo@softtek.com>
2021-10-04 09:35:39 -0300

    ch1

--- webapp/controller/Carpeta.controller.js
+++ webapp/controller/Carpeta.controller.js
@@ -2,9 +2,10 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/model/json/JSONModel",
 	"../model/formatter",
+    "sap/m/Link",
 	"sap/ui/model/Filter",
 	"sap/ui/model/FilterOperator"
-], function (Controller, JSONModel, formatter, Filter, FilterOperator) {
+], function (Controller, JSONModel, formatter, Link, Filter, FilterOperator) {
 	"use strict";
 
 	return Controller.extend("profertil.instructivos.controller.Carpeta", {
@@ -20,26 +21,75 @@ sap.ui.define([
 		 * @public
 		 */
 		onInit : function () {
+
+            var sPath = "PROCEDIMIENTOS_E_INSTRUCTIVOS";
+
 			var oViewModel,			
 			oViewModel = new JSONModel({
                 rootUrl: "",
-                rootFolder: ""
+                rootFolder: "",
+                repositoryId: "",
+                repositoryName: sPath,
+                repositoryDescription: "",
 			});
 			this.getView().setModel(oViewModel, "viewModel");
+            this.createFolderModel();
             this.setRepoUrl();
+
+            var oBreadCrumb = this.byId("breadcrumb");
+			var oLink = new Link({
+				text: sPath,
+				press:[sPath, this.onBreadcrumbPress, this]
+			});
+			oBreadCrumb.addLink(oLink);
 
 		},
 
 		onPress : function (oEvent) {
+            var oContext = oEvent.getSource().getBindingContext("folderModel");
+            //var sName = this.getCmisObjectValue(oContext, "cmis:name");
+            var name = this.getCmisObjectName(oContext);
+            var objectType = this.getCmisObjectType(oContext);
+            var objectId = this.getCmisObjectId(oContext);
+            sap.m.MessageToast.show(name + " - " + objectType + " - " + objectId);
+
+            if (objectType === "cmis:folder") {
+                var sPath = this.getView().getModel("viewModel").getProperty("/currentPath") + "/" + name;
+                this.getFolderObjects(sPath);
+                //this.getFolderObjects("F");
+            } else if (objectType === "cmis:document") {
+                //sap.m.MessageToast.show("Document");
+            }
+            
+
 			// The source is the list item that got pressed
-			this._showObject(oEvent.getSource());
+			//this._showObject(oEvent.getSource());
 		},
 
-		_showObject : function (oItem) {
-			this.getRouter().navTo("object", {
-				objectId: oItem.getBindingContext().getProperty("Id")
-			});
-		},
+
+        getCmisObjectName: function (oContext) {
+            return this.getCmisObjectValue(oContext, "cmis:name");
+        },
+        getCmisObjectId: function (oContext) {
+            return this.getCmisObjectValue(oContext, "cmis:objectId");
+        },
+        getCmisObjectType: function (oContext) {
+            return this.getCmisObjectValue(oContext, "cmis:objectTypeId");
+        },                                            
+        getCmisParentId: function (oContext) {
+            return this.getCmisObjectValue(oContext, "sap:parentIds");
+        },        
+        getCmisObjectValue: function (oContext, sTag) {
+            var sPath=oContext.getPath();
+            var oCmisObject = oContext.getProperty(sPath); 
+            return oCmisObject.object.properties[sTag].value;           
+        },
+
+
+        createFolderModel: function () {
+            var oModel = new JSONModel();
+            this.getView().setModel(oModel, "folderModel");
+        },
 
 
         getDMSUrl: function (sPath) {
@@ -49,31 +99,73 @@ sap.ui.define([
 
 
         getData: function (path) {
-            var url = this.getDMSUrl("/SDM_API/browser");
-            var fullUrl = path ? url + "/" + path : url;
-            return $.get({url: fullUrl});
+            //var url = this.getDMSUrl("/SDM_API/browser");
+            //var fullUrl = path ? url + "/" + path : url;
+            //return $.get({url: fullUrl});
+
+            return new Promise(function (resolve, reject) {
+                
+                var oResponse = new sap.ui.model.json.JSONModel();
+                oResponse.attachRequestCompleted(function() {
+                    resolve(oResponse.getData());
+                });
+
+                if (path === "") {
+                    oResponse.loadData("../localService/xroot.json"); 
+                } else if (path === "C"){
+                    oResponse.loadData("../localService/xfolders.json"); 
+                } else if (path === "F") {
+                    oResponse.loadData("../localService/xfiles.json");
+                } else {
+                    oResponse.loadData("../localService/xfolders.json");
+                }
+                //resolve(oResponse.getJSON());
+
+            });
 
         },
 
         setRepoUrl: function () {
+            var sRepositoryName = this.getView().getModel("viewModel").getProperty("/repositoryName"); 
             var that = this;
             this.getData("").then(response => {
-                var repos = Object.keys(response).filter(repo => response[repo].repositoryName == "PROCEDIMIENTOS_E_INSTRUCTIVOS");
+                var repos = Object.keys(response).filter(repo => response[repo].repositoryName == sRepositoryName);
                 var root = repos[0] + "/root";
                 var url = this.getDMSUrl("/SDM_API/browser/" + root);
-                //this._dmsUrl = url;
+                //
                 that.getView().getModel("viewModel").setProperty("/rootUrl", url);
                 that.getView().getModel("viewModel").setProperty("/rootFolder", root);
-                that.getFolderObjects(root);
+                that.getView().getModel("viewModel").setProperty("/currentPath", root);
+                that.getView().getModel("viewModel").setProperty("/repositoryId", response[repos[0]].repositoryId);
+                that.getView().getModel("viewModel").setProperty("/repositoryName", response[repos[0]].repositoryName);
+                that.getView().getModel("viewModel").setProperty("/repositoryDescription", response[repos[0]].repositoryDescription);
+                that.getFolderObjects(root);  
             });
 
         },
 
         getFolderObjects: function (sPath) {
+            var that = this;
             this.getData(sPath).then(response => {
-                console.warn("objects: " + response.objects.length);
+                that.getView().getModel("folderModel").setData(response);
             })
         },
+
+
+
+        onBreadcrumbPress: function (oEvent, sPath) {
+			var oLink = oEvent.getSource();
+			var oBreadCrumb = this.byId("breadcrumb");
+			var iIndex = oBreadCrumb.indexOfLink(oLink);
+			var aCrumb = oBreadCrumb.getLinks().slice(iIndex + 1);
+			if (aCrumb.length) {
+				aCrumb.forEach(function(oLink) {
+					oLink.destroy();
+				});
+				//this._setAggregation(sPath);
+			}
+		},
+
 
         getAdjuntos: function (oResponse) {
             var aObjects = [];
--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -8,6 +8,69 @@ sap.ui.define([], function () {
 				return "";
 			}
 			return parseFloat(sValue).toFixed(2);
+		},
+
+		sizeVisible: function (sType) {
+			return (sType === "cmis:document") ? true : false;
+		},
+
+		formatMSDate: function (sMSDate) {
+			var d = new Date(sMSDate);
+			//return d.toDateString();
+			return d.toLocaleDateString();
+
+		},
+
+		formatObjectType: function (sName, sType) {
+			var sIcon = "sap-icon://document";
+			var sExtension = sName.split('.').pop();
+			
+			if (sType === "cmis:document") {
+				switch (sExtension) { 
+					case 'pdf':
+						sIcon = "sap-icon://pdf-attachment";
+						break;
+					case 'xls':
+					case 'xlsx':
+						sIcon = "sap-icon://excel-attachment";
+						break;
+					case 'ppt':
+						sIcon = "sap-icon://ppt-attachment";
+						break;
+					case 'txt':
+						sIcon = "sap-icon://attachment-text-file";
+						break;
+					case 'png':
+					case 'jpg':
+					case 'jpeg':
+					case 'bmp':
+						sIcon = "sap-icon://attachment-photo";
+						break;
+					case 'doc':
+					case 'docx':
+						sIcon = "sap-icon://doc-attachment";
+						break;
+					case 'zip':
+						sIcon = "sap-icon://attachment-zip-file";
+						break;
+					case 'mp4': 
+					case 'mpeg': 
+						sIcon = "sap-icon://attachment-video";
+						break;
+					case 'opus': 
+					case 'mp3':
+						sIcon = "sap-icon://attachment-audio";
+						break;
+
+				}
+				
+			}
+
+			if (sType === "cmis:folder") {
+				sIcon = "sap-icon://open-folder";
+			}
+
+			return sIcon;
 		}
 
         
